6b6f438867d385f0dabc9cae7e131bb16e4c83b2
Szzng <seungheeseo53@gmail.com>
2022-05-24 23:50:28 +0900

    BugFix

--- FrontEnd/src/components/Tablets/CheckTabletsBookingDialog.vue
+++ FrontEnd/src/components/Tablets/CheckTabletsBookingDialog.vue
@@ -1,9 +1,43 @@
 <template>
   <div class="mt-1">
-    <v-sheet
-      v-show="dialog.checkTabletsBooking"
-      outlined
-    >
+    <v-sheet v-show="!dialog.checkTabletsBooking || !focusDate" outlined>
+      <v-card class="pl-5" flat>
+        <v-card-actions class="mb-3">
+          <v-spacer></v-spacer>
+          <v-card-title class="pl-0"
+            >달력에서 예약 날짜를 선택하세요.</v-card-title
+          >
+          <v-spacer></v-spacer>
+        </v-card-actions>
+
+        <v-card v-for="period in periods" :key="period" flat class="my-0 py-0">
+          <v-row align="center">
+            <v-col sm="3" md="3">
+              <v-card-subtitle class="pb-0 black--text">
+                {{ period }}교시<br />
+                <span class="purple--text">+ 잔여 대수</span>
+              </v-card-subtitle>
+            </v-col>
+
+            <v-col class="pa-0">
+              <v-card-text>
+                <v-chip-group column>
+                  <v-chip color="primary" outlined>
+                    <v-avatar left>
+                      <v-icon>mdi-checkbox-marked-circle</v-icon>
+                    </v-avatar>
+                    예약자 (OO대)
+                  </v-chip>
+                </v-chip-group>
+              </v-card-text>
+            </v-col>
+          </v-row>
+          <v-divider class="my-3 mr-7"></v-divider>
+        </v-card>
+      </v-card>
+    </v-sheet>
+
+    <v-sheet v-show="dialog.checkTabletsBooking" outlined>
       <v-card class="pl-5" flat>
         <v-card-actions class="mb-3">
           <v-spacer></v-spacer>
