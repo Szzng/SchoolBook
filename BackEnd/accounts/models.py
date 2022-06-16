46362e105624e8f50bd1ea3a40eb24fa0f303ece
Szzng <seungheeseo53@gmail.com>
2022-06-16 23:50:20 +0900

    School 모델에서 code 삭제, name을 pk로 사용

--- BackEnd/accounts/models.py
+++ BackEnd/accounts/models.py
@@ -1,14 +1,6 @@
-import random
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.db import models
-
-
-def create_school_code():
-    code = random.randint(100, 9999)
-    while School.objects.filter(code=code).exists():
-        code = random.randint(100, 9999)
-    return code
 
 
 class SchoolUserManager(BaseUserManager):
@@ -19,37 +11,35 @@ class SchoolUserManager(BaseUserManager):
             raise ValueError("학교명을 입력해주세요.")
 
         school = self.model(name=name)
-        school.code = create_school_code()
         school.password = make_password(password)
         school.save(using=self._db)
         return school
 
-    def create_superuser(self, code, password):
-        school = self.model(name='SuperUser', code=code)
-        school.password = make_password(password)
-        school.is_staff = True
-        school.is_superuser = True
-        school.save(using=self._db)
-        return school
+    def create_superuser(self, name, password):
+        superuser = self.create_user(name, password)
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.save(using=self._db)
+        return superuser
 
 
 class School(AbstractBaseUser, PermissionsMixin):
     objects = SchoolUserManager()
 
-    name = models.CharField(max_length=30)
-    code = models.PositiveBigIntegerField(primary_key=True)
+    name = models.CharField(max_length=30, primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-    USERNAME_FIELD = "code"
+    USERNAME_FIELD = "name"
 
     def __str__(self):
-        return self.name + '/' + str(self.code)
+        return self.name
 
 
 class Ip(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     ip = models.CharField(max_length=16, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
