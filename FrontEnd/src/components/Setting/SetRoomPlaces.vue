edc12e373b24aa00c7e4923c35ffa771127324ea
Szzng <seungheeseo53@gmail.com>
2022-06-21 20:15:18 +0900

    cssFix

--- FrontEnd/src/components/Setting/SetRoomPlaces.vue
+++ FrontEnd/src/components/Setting/SetRoomPlaces.vue
@@ -1,87 +1,96 @@
 <template>
-  <div class="mt-4">
-    <v-sheet class="mx-16">
-      <v-row align="start" justify="center">
-        <v-col sm="12" md="6">
-          <v-card class="mt-1 mb-5" outlined>
-            <v-card-title>등록된 교실 · 장소 수정</v-card-title>
-            <v-card-text class="mt-3">
-              <v-card
-                v-for="place in rooms"
-                :key="place.name"
+  <div class="mt-4 mx-16">
+    <v-card class="mx-10 mt-5" flat>
+      <v-row justify="center" align="center">
+        <v-card-title style="font-size: 22px" class="accent--text">
+          <v-icon class="mr-2" large color="accent"
+            >mdi-google-classroom</v-icon
+          >
+          예약제를 운영할 교실 · 장소를 등록하고 관리하세요.
+          <v-icon class="ml-2" large color="accent"
+            >mdi-google-classroom</v-icon
+          >
+        </v-card-title>
+      </v-row>
+    </v-card>
+
+    <v-card class="my-10" outlined>
+      <v-card-title>
+        <v-row align="center" justify="start">
+          <v-col sm="6" md="9" class="pb-0">
+            <v-form ref="form" lazy-validation>
+              <v-text-field
+                v-model="newRoom"
                 outlined
-                class="py-2 mb-3"
+                label="교실 · 장소 이름을 입력하세요. (특수 문자 불가)"
+                :rules="placeNameRule"
+              ></v-text-field>
+            </v-form>
+          </v-col>
+          <v-col sm="6" md="3" class="pl-0 pb-7">
+            <v-btn
+              :disabled="disabled"
+              outlined
+              x-large
+              block
+              color="success"
+              elevation="3"
+              @click="createTimetable"
+              >기본 시간표 등록
+              <v-icon class="ml-1">mdi-timetable</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-title>
+    </v-card>
+
+    <v-card class="my-10" flat>
+      <v-row>
+        <v-col
+          sm="6"
+          md="4"
+          v-for="place in rooms"
+          :key="place.name"
+          class="my-3"
+        >
+          <v-card outlined class="py-3 px-2">
+            <v-row justify="center" align="center">
+              <v-card-title style="font-size: 20px; font-weight: bolder">
+                <v-icon class="mx-0 mb-1" large color="accent"
+                  >mdi-cradle</v-icon
+                >
+
+                {{ place.name }}</v-card-title
               >
-                <v-row align="center" justify="start">
-                  <v-col sm="12" md="4" class="pl-7">
-                    <span style="font-size: 18px; font-weight: bolder">
-                      {{ place.name }}</span
-                    >
-                  </v-col>
-
-                  <v-col sm="12" md="8" class="text-right pr-6">
-                    <v-btn
-                      class="mr-2"
-                      width="120"
-                      height="45"
-                      outlined
-                      color="success"
-                      elevation="2"
-                      @click="updateTimetable(place.name)"
-                      >기본 시간표
-                      <v-icon>mdi-timetable</v-icon>
-                    </v-btn>
-                    <v-btn
-                      width="70"
-                      height="45"
-                      outlined
-                      color="secondary"
-                      elevation="2"
-                      @click="destroyRoom(place.name)"
-                      >삭제
-                      <v-icon>mdi-delete-alert-outline</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-card>
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col sm="12" md="6">
-          <v-card class="mt-1 mb-5" outlined>
-            <v-card-title>교실 · 장소 추가 등록</v-card-title>
-            <v-card-title>
-              <v-row align="center" justify="start">
-                <v-col sm="6" md="12">
-                  <v-form ref="form" lazy-validation>
-                    <v-text-field
-                      v-model="newRoom"
-                      outlined
-                      label="교실 · 장소 이름을 입력하세요. (특수 문자 불가)"
-                      :rules="placeNameRule"
-                    ></v-text-field>
-                  </v-form>
-                </v-col>
-                <v-col sm="6" md="12">
-                  <v-btn
-                    :disabled="disabled"
-                    outlined
-                    x-large
-                    block
-                    color="success"
-                    elevation="3"
-                    @click="createTimetable"
-                    >기본 시간표 등록
-                    <v-icon class="ml-1">mdi-timetable</v-icon>
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-card-title>
+            </v-row>
+            <v-row justify="center" align="center" class="mb-3">
+              <v-btn
+                class="mr-2"
+                width="120"
+                height="45"
+                outlined
+                color="success"
+                elevation="2"
+                @click="updateTimetable(place.name)"
+                >기본 시간표
+                <v-icon>mdi-timetable</v-icon>
+              </v-btn>
+              <v-btn
+                width="70"
+                height="45"
+                outlined
+                color="secondary"
+                elevation="2"
+                @click="destroyRoom(place.name)"
+                >삭제
+                <v-icon>mdi-delete-alert-outline</v-icon>
+              </v-btn>
+            </v-row>
           </v-card>
         </v-col>
       </v-row>
-    </v-sheet>
+    </v-card>
+
     <UpdateTimetableDialog :room="room" />
     <DestroyRoomDialog :room="room" />
 
